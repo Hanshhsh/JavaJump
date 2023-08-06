6937e4bff9ede2add007c5ec79df4e59bfe97a86
Hanshhsh <killshot020542@gmail.com>
2023-08-06 21:02:12 +0900

    cute helloworld & variable

---
+++ src/TypeOfType.java
@@ -0,0 +1,21 @@
+public class TypeOfType {
+    public static void main(String[] args) {
+        System.out.println();       // Number
+        System.out.println("six");  // String
+        System.out.println("6");    // String
+
+        System.out.println(6+6);
+        System.out.println("6"+6);
+
+        System.out.println("1444".length());
+
+        System.out.println("Hello \n \"world\"");
+        System.out.println("Hello! [name]. Do you know \"Java\"?".replace("[name]", "suprem"));
+
+        String name ="boostCourse";
+        System.out.println(name);
+
+        name="hello";
+        System.out.println(name);
+    }
+}
---
+++ src/Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        int a = 1; // Number -> integer ...
+        System.out.println(a);
+
+        double b = 1.1;
+        System.out.println(b);
+
+        float c = 0L;
+        System.out.print(c);
+
+        String d = "Hello World";
+        System.out.print(d);
+
+    }
+}
